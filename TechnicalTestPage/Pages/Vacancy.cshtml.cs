65227d3f590267d647faee0ae7eff32497116318
Jaime Tenorio <jaimetenorio_geografia@outlook.es>
2023-05-07 13:01:13 -0500

    Vista crud para las vacantes finalizada

---
+++ TechnicalTestPage/Pages/Vacancy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TechnicalTestPage.Pages
+{
+    public class VacancyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
